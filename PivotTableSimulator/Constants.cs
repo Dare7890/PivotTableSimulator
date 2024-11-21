b16e0ce95b7e03d5054c618e22c964d4c94b6dbf
Fetisov Vladislav <vladfetisov2011@yandex.ru>
2024-11-22 00:47:25 +0300

    refactor: build resource file path

--- PivotTableSimulator/Constants.cs
+++ PivotTableSimulator/Constants.cs
@@ -2,10 +2,16 @@
 {
     public static class Constants
     {
-        public static readonly string FILES_FOLDER_PATH = $"{Environment.CurrentDirectory}\\..\\..\\..\\..\\Resources";
-        public static readonly string MERGED_FILE_PATH = $"{Environment.CurrentDirectory}\\..\\..\\..\\..\\Resources\\Merged_Table.xlsx";
-        public static readonly string PIVOT_TABLE_FILE_PATH = $"{Environment.CurrentDirectory}\\..\\..\\..\\..\\Resources\\PivotTable_Table.xlsx";
+        public static readonly string FILES_FOLDER_PATH = GetResourcesFullPath(string.Empty);
+        public static readonly string MERGED_FILE_PATH = GetResourcesFullPath("Merged_Table.xlsx");
+        public static readonly string PIVOT_TABLE_FILE_PATH = GetResourcesFullPath("PivotTable_Table.xlsx");
 
         public static readonly string RESULT_SHEET_NAME = "resultSheet";
+
+        private static string GetResourcesFullPath(string fileName)
+        {
+            return $"{Environment.CurrentDirectory}\\..\\..\\..\\..\\Resources" +
+                (string.IsNullOrEmpty(fileName) ? string.Empty : string.Format("\\{0}", fileName));
+        }
     }
 }
